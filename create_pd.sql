2786bee5f918ad5fa24bfecf2e65c3ca2e617c32
Hongsu Wang <sudospace@gmail.com>
2023-06-06 18:54:26 -0400

    Update create_pd.sql

--- create_pd.sql
+++ create_pd.sql
@@ -1,8 +1,8 @@
-SELECT BIOG_MAIN.c_personid, BIOG_MAIN.c_name_chn, 
+SELECT BIOG_MAIN.c_personid as person_id, BIOG_MAIN.c_name_chn as person_name, 
 CASE
     WHEN BIOG_MAIN.c_female= 0 THEN '男'
     WHEN BIOG_MAIN.c_female<> 0 THEN '女'
-END as c_female,
-BIOG_MAIN.c_birthyear, BIOG_MAIN.c_deathyear, DYNASTIES.c_dynasty_chn, ADDR_CODES.c_name_chn
+END as gender,
+BIOG_MAIN.c_birthyear as born_year, BIOG_MAIN.c_deathyear as died_year, DYNASTIES.c_dynasty_chn as dynasty, ADDR_CODES.c_name_chn as jiguan
 FROM BIOG_MAIN LEFT JOIN ADDR_CODES ON BIOG_MAIN.c_index_addr_id = ADDR_CODES.c_addr_id LEFT JOIN DYNASTIES ON DYNASTIES.c_dy = BIOG_MAIN.c_dy
 WHERE c_personid != 0
